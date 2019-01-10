65d25b31e48c6acf33116c6914b31d83a1d24e7d
didi0410 <taristmas@gmail.com>
2019-01-10 15:37:27 +0900

    2

--- BaedalInUs/app.js
+++ BaedalInUs/app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000!');
+});
+
 module.exports = app;
