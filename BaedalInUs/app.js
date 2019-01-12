df7cb7467203754d7f738af036c497e757043c75
didi0410 <taristmas@gmail.com>
2019-01-12 17:28:25 +0900

    123

--- BaedalInUs/app.js
+++ BaedalInUs/app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//asd
 
 app.use(logger('dev'));
 app.use(express.json());
