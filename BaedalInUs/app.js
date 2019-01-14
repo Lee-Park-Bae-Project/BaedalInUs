fbc47dc326b8ce336a0b571da4a27bcaf8612248
didi0410 <taristmas@gmail.com>
2019-01-14 15:07:09 +0900

    mongodb 연결, CRUD 대충 연습

--- BaedalInUs/app.js
+++ BaedalInUs/app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +43,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(3000, function(){
-  console.log('Example app listening on port 3000!');
+  console.log('Baedal in us listening on port 3000!');
 });
 
 module.exports = app;
---
+++ BaedalInUs/routes/test.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error', function () {
+    console.log('Connection Failed!');
+});
+
+db.once('open', function () {
+    console.log('Connected!');
+
+});
+
+/*
+Create: 데이터 생성
+Retrieve(Read): 데이터 조회
+Update: 데이터 갱신
+Delete: 데이터 삭제
+ */
+
+/*
+이러한 구조의 데이터를 넣곘다.
+ */
+const testSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String
+});
+
+/*
+testModel : 데이터가 들어갈 collection 이름
+testSchema 형태의 데이터를 넣겠다.
+ */
+const data = mongoose.model('testModel', testSchema);
+
+router.get('/', function (req, res) {
+    res.send('mongodb test page');
+});
+
+router.get('/create', function (req, res) {
+    data.find({}, (err, myData) => {
+        if (err) return res.json(err);
+    });
+
+
+    var newData = new data({firstname: "junjun", lastname: "leelee"});
+    newData.save(); // 데이터 저장
+
+
+    data.find({}, (err, myData) => {
+        if (err) return res.json(err);
+    });
+
+    res.send('create is done');
+
+});
+
+/*
+data collection 에서 데이터 다 가져옴
+ */
+router.get('/retrieve', function (req, res) {
+    data.find({}, (err, myData) => {
+        if (err) return res.json(err);
+        res.json(myData);
+    });
+});
+
+/*
+data collection 에 있는 데이터 다 지움
+ */
+router.get('/delete', function(req, res){
+    data.remove({}, (err, myData)=>{
+        if(err) return res.json(err);
+        res.redirect('/');
+    });
+});
+
+/*
+firstname이 junjun인 애를 updatedData 로 업데이트함
+ */
+router.get('/update', function(req,res){
+    updatedData={
+        firstname:'updated firstname',
+        lastname: 'updated lastname'
+    };
+
+    data.update({firstname:"junjun"}, updatedData, function(err, myData){
+        if(err) return res.json(err);
+        res.redirect('/');
+    });
+});
+
+
+
+module.exports = router;
