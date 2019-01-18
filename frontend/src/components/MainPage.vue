2a6e6817a796564bccedc0850b8af7d7478975bc
didi0410 <taristmas@gmail.com>
2019-01-18 19:07:48 +0900

    add mainPage

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="main">
+    <div>Login</div>
+    <button v-on:click="login">logout</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "MainPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
-
 import IndexPage from '@/components/IndexPage.vue'
 import LoginPage from '@/components/LoginPage.vue'
 import SignUpPage from '@/components/SignUpPage.vue'
+Vue.use(Router)
+
+
 export default new Router({
   mode: 'history',
   routes: [
@@ -24,6 +24,11 @@ export default new Router({
       path:'/signup',
       name:'SignUpPage',
       component:SignUpPage
+    },
+    {
+      path:'/main',
+      name:'MainPage',
+      component:MainPage
     }
 
   ]
