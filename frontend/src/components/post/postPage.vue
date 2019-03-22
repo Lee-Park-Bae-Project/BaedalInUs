47300fa0e2bb30172b30902411473ecc313f43e8
HanseopShin <hanseop95@gmail.com>
2019-03-22 10:50:19 +0900

    change post page design

--- frontend/src/components/post/postPage.vue
+++ frontend/src/components/post/postPage.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="outer">
-    <div class = "inner">
-      <div class="form_box">
-        <h1> Post your order</h1>
-        <input v-model="board.title" placeholder="제목"><br/>
-        <input v-model="board.content" placeholder="주문 내용"><br/>
-        <input v-model="board.fee" placeholder="수수료"><br/>
-        <input id="addrValue" @click="searchAddress" v-model="board.addr" placeholder="주소" readonly><br/>
-        <!-- <input id="addrValue" v-model="board.addr" placeholder="주소"><br/>-->
+  <body>
+  <div class="container">
 
-        <input id="detailedAddr" v-model="board.detailedAddr" placeholder="상세 주소"><br/>
+    <form class="form-post">
+      <h1 class="form-post-heading">Post your order</h1>
+      <input v-model="board.title" placeholder="제목" id="title" class="form-control" required autofocus>
+      <input v-model="board.content" placeholder="주문 내용" type="text"
+             class="form-control" required>
+      <input v-model="board.fee" placeholder="수수료" type="text" class="form-control"
+             style="border-radius: 0; margin-top: -1px" required>
+      <input id="addrValue" @click="searchAddress" v-model="board.addr" placeholder="주소" readonly type="text"
+             class="form-control" required>
+      <input id="detailedAddr" v-model="board.detailedAddr" placeholder="상세 주소" class="form-control">
+      <button @click="post" class="submit btn btn-lg btn-success btn-block">Submit</button>
+    </form>
 
-        <button @click="post">Submit</button>
-      </div>
+  </div> <!-- /container -->
+  </body>
+  <!--<div class="outer">-->
+  <!--<div class="inner">-->
+  <!--<div class="form_box">-->
+  <!--<h1> Post your order</h1>-->
+  <!--<input v-model="board.title" placeholder="제목"><br/>-->
+  <!--<input v-model="board.content" placeholder="주문 내용"><br/>-->
+  <!--<input v-model="board.fee" placeholder="수수료"><br/>-->
+  <!--&lt;!&ndash;<input id="addrValue" @click="searchAddress" v-model="board.addr" placeholder="주소" readonly><br/>&ndash;&gt;-->
+  <!--&lt;!&ndash; <input id="addrValue" v-model="board.addr" placeholder="주소"><br/>&ndash;&gt;-->
 
-    </div>
-  </div>
+  <!--&lt;!&ndash;<input id="detailedAddr" v-model="board.detailedAddr" placeholder="상세 주소"><br/>&ndash;&gt;-->
+
+  <!--<button @click="post">Submit</button>-->
+  <!--</div>-->
+
+  <!--</div>-->
+  <!--</div>-->
 </template>
 
 <script>
@@ -25,9 +43,9 @@
       ckeditor.setAttribute('src', "http://dmaps.daum.net/map_js_init/postcode.v2.js?autoload=false");
       document.head.appendChild(ckeditor);
     },
-    mounted(){
+    mounted() {
       console.log('hello');
-      this.board.userID = this.$store.getters.getUserID;      
+      this.board.userID = this.$store.getters.getUserID;
     },
     name: 'postPage',
     data: function () {
@@ -38,11 +56,11 @@
           content: '',
           fee: '',
           addr: '',
-          detailedAddr:'',
+          detailedAddr: '',
           dueDate: '',
-          lat:'',
-          lng:'',
-          userID:'',
+          lat: '',
+          lng: '',
+          userID: '',
 
         },
       }
@@ -51,7 +69,7 @@
       post: function (event) {
         let that = this;
 
-        if(this.board.detailedAddr.length === 0){
+        if (this.board.detailedAddr.length === 0) {
           alert('상세 주소를 입력해주세요.');
         }
 
@@ -62,7 +80,7 @@
             (response) => {
               if (response.status === 200) {
                 console.log(response);
-                if(response.data.complete){
+                if (response.data.complete) {
 
                   alert('주문이 완료되었습니다.');
                   this.$router.push('/pagination');
@@ -112,11 +130,11 @@
           }).open();
         });
       },
-      setLatLng:function(addr){
+      setLatLng: function (addr) {
         let that = this;
         var geocoder = new daum.maps.services.Geocoder();
 
-        var callback = function(result, status) {
+        var callback = function (result, status) {
           if (status === daum.maps.services.Status.OK) {
             console.log(result);
             that.board.lat = result[0].y;
@@ -130,34 +148,78 @@
   }
 </script>
 <style scoped>
-
-  .outer {
-    font-family: "Franklin Gothic Demi";
-    width: 100%;
-    text-align: center;
+  body {
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #F8F8F9;
   }
 
-  .inner {
-    display: inline-block;
+  .form-post {
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
   }
 
-  .form_box {
-    margin-top: 150px;
-    text-align: left;
+  .form-post .form-post-heading {
+    margin-bottom: 10px;
   }
 
-  h1 {
-    margin: 10px;
+
+  .form-post .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
   }
 
-  input {
-    width: 300px;
-    margin: 10px;
+  .form-post .form-control:focus {
+    z-index: 2;
   }
 
-  button {
-    float: right;
-    margin: 10px;
-    width: 100px;
+  #title {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
   }
+
+  #detailedAddr {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+
+  .form-post input[type="text"]{
+    border-radius: 0;
+  }
+
+  /*.outer {*/
+  /*font-family: "Franklin Gothic Demi";*/
+  /*width: 100%;*/
+  /*text-align: center;*/
+  /*}*/
+
+  /*.inner {*/
+  /*display: inline-block;*/
+  /*}*/
+
+  /*.form_box {*/
+  /*margin-top: 150px;*/
+  /*text-align: left;*/
+  /*}*/
+
+  /*h1 {*/
+  /*margin: 10px;*/
+  /*}*/
+
+  /*input {*/
+  /*width: 300px;*/
+  /*margin: 10px;*/
+  /*}*/
+
+  /*button {*/
+  /*float: right;*/
+  /*margin: 10px;*/
+  /*width: 100px;*/
+  /*}*/
 </style>
